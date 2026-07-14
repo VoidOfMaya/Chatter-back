b5afc668a183a2d249b31d0a38cee06304682075
Maya <MayaOfTheVoid@gmail.com>
2026-07-14 10:14:51 +0300

    emit response form server to client git push origin ws

--- features/sockets/onlineStatus.js
+++ features/sockets/onlineStatus.js
@@ -1,17 +1,17 @@
 import { Server } from 'socket.io';
 //controller
-const setOnlineStatus = (socket) =>{
+const setOnlineStatus =(socket) =>{
     let user= null;
     socket.on('is_online',(data)=>{
         user = data.id
-        console.log(`user ${data.userId}loggerd in`)
-    })
-    setTimeout(()=>{
-        socket.emit('response',()=>{
+        console.log(`user ${data.userId}logged in`)
+        setTimeout(()=>{
             console.log(`sending response`)
-            return {message: `user at ${user}, PONG response`}
-        })
-    },5000)
+            socket.emit('response',{
+                message: `user at ${data.userId}, PONG response`
+            })
+        },5000)
+    })
     //console.log(`user ${data.userId}loggerd in`)
 }
 export{
