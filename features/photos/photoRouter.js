faca614a21687df5193d24b9a4e8f4a4aab9c371
Maya <MayaOfTheVoid@gmail.com>
2026-07-22 11:37:17 +0300

    remove unused photoMsg route

--- features/photos/photoRouter.js
+++ features/photos/photoRouter.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { uploadFile,msgPhoto } from "./photoController.js";
+import { uploadFile } from "./photoController.js";
 import { multerMiddleware } from "./photoMiddleWare.js";
 
 const photoRouter = Router();
 
 photoRouter.post('/file',multerMiddleware,uploadFile);
-photoRouter.post('/msg',multerMiddleware,uploadFile);
+//photoRouter.post('/msg',multerMiddleware,uploadFile);
 
 export{
     photoRouter
